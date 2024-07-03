d5d6d80559288c658a09390297469265b3fbe2ef
Steve Mathew <mathewsteve68@gmail.com>
2024-07-03 16:56:51 -0400

    completed project

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-var slideElem = document.getElementById("charSlider");
-var slideLength = document.getElementById("charLength");
 
+// Function that generates the password
+function generatePassword(pswdlength, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
 
-
-
-
-function generatePassword(){
-
+    //pool of letters numbers and symbols the password will be made from
     const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
     const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const numberChars = "1234567890";
     const symbolChars = "!@#$%^&*()_+-=";
 
+      //Inittially, password is empty. Allowed characters are also empty as no checkboxes are checked
     let allowedChars = "";
     let password = "";
 
-    allowedChars += includeLowercase ? lowercaseChars : "";
-    allowedChars += includeUppercase ? uppercaseChars : "";
-    allowedChars += includeNumbers ? numberChars : "";
-    allowedChars += includeSymbols ? symbolChars : "";
+    if (includeLowercase) allowedChars += lowercaseChars;
+    if (includeUppercase) allowedChars += uppercaseChars;
+    if (includeNumbers) allowedChars += numberChars;
+    if (includeSymbols) allowedChars += symbolChars;
 
     if(pswdlength <= 0){
         return `(Password length must be atleast 1)`;
@@ -28,6 +25,7 @@ function generatePassword(){
         return `(At least 1 set of character needs to be selected)`;
     }
 
+    //for loop returns each letter randomly from the options until pswdlength requirements are met
     for(let i = 0; i < pswdlength; i++){
         const randomIndex = Math.floor(Math.random()*allowedChars.length);
         password += allowedChars[randomIndex];
@@ -37,16 +35,48 @@ function generatePassword(){
     return password;
 }
 
+window.addEventListener('DOMContentLoaded', (event) => {
+  //each component is acquired from the html file
+  let pswdlength = parseInt(document.getElementById("charLength").value);
+  let upperBox = document.getElementById("upper");
+  let lowerBox = document.getElementById("lower");
+  let numberBox = document.getElementById("number");
+  let symbolBox = document.getElementById("symbol");
 
+  let includeLowercase = lowerBox.checked;
+  let includeUppercase = upperBox.checked;
+  let includeNumbers = numberBox.checked;
+  let includeSymbols = symbolBox.checked;
 
-const pswdlength = 12;
-const includeLowercase = true;
-const includeUppercase = true;
-const includeNumbers = true;
-const includeSymbols = true;
+  //eventListeners and their corresponding actions
+  lowerBox.addEventListener('change', function () {
+    includeLowercase = this.checked;
+  });
+  upperBox.addEventListener('change', function () {
+    includeUppercase = this.checked;
+  });
+  numberBox.addEventListener('change', function () {
+    includeNumbers = this.checked;
+  });
+  symbolBox.addEventListener('change', function () {
+    includeSymbols = this.checked;
+  });
 
-const password = generatePassword(
-    pswdlength, includeLowercase, includeUppercase,
-    includeNumbers, includeSymbols);
+  //redo button click function
+  document.getElementById("redo").addEventListener("click", function () {
+    pswdlength = parseInt(document.getElementById("charLength").value);
+    const password = generatePassword(pswdlength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+    document.getElementById("result").value = password;
+  });
 
-console.log(`Generated Password: ${password}`);
+  //copy button click function
+  document.getElementById("copy").addEventListener("click", function () {
+    const resultInput = document.getElementById("result");
+    resultInput.select();
+    document.execCommand("copy");
+    alert("Password copied to clipboard");
+  });
+  // Generate a password when the page loads
+  const initialPassword = generatePassword(pswdlength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+  document.getElementById("result").value = initialPassword;
+});
